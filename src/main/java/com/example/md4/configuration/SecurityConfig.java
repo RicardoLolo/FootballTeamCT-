4b4a6bbbb3d192743ec71fe9426b1adaa861bb2b
RicardoLolo <loloringo9999@gmail.com>
2022-03-17 19:59:02 +0700

    20:00

--- src/main/java/com/example/md4/configuration/SecurityConfig.java
+++ src/main/java/com/example/md4/configuration/SecurityConfig.java
@@ -49,22 +49,48 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManager();
     }
 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.cors().and().csrf().disable().authorizeRequests()
+//                .antMatchers("/main/webapp/","/api/login").permitAll()
+//                .antMatchers("/index/**",
+//                        "/calendar/**",
+//                        "/icons/**",
+//                        "/tables/**").authenticated()
+//                .antMatchers("/api/forms").hasAnyAuthority("ROLE_ADMIN")
+//                .antMatchers("/profile_player").hasAnyAuthority("ROLE_PLAYER")
+//                .antMatchers("/profile_coach").hasAnyAuthority("ROLE_COACH")
+//                .anyRequest().authenticated()
+//                .and()
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll()
+//                .and()
+//                .exceptionHandling()
+//                .authenticationEntryPoint(restAuthenticationEntryPoint)
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+//    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers("/","/api/login",
-                        "/index/**",
+        http.authorizeRequests().antMatchers("/index/**",
                         "/calendar/**",
                         "/icons/**",
                         "/tables/**").permitAll()
-                .antMatchers("/api/forms").hasAnyAuthority("Role_Admin")
-                .antMatchers("/profile_player").hasAnyAuthority("Role_Player")
-                .antMatchers("/profile_coach").hasAnyAuthority("Role_Coach")
-                .anyRequest().authenticated()
-
+                .and().authorizeRequests()
+                .antMatchers("/forms**").hasRole("ADMIN")
+                .and().authorizeRequests()
+                .antMatchers("/profile_player**").hasRole("PLAYER")
+                .and().authorizeRequests()
+                .antMatchers("/profile_coach**").hasRole("COACH ")
                 .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll()
+                .formLogin().loginPage("/login").permitAll().successHandler(new CustomSuccessHandler())
+                .and()
+                .csrf().disable().exceptionHandling().accessDeniedPage("/accessDenied")
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll()
                 .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(restAuthenticationEntryPoint)
